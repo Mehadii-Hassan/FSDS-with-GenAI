d91918feee95c5c3a4b36a9fabfce3c799fc8d8b
Mehadii-Hassan <mehadi.cse01@gmail.com>
2025-12-18 13:44:30 +0600

    updated

--- Practice/class/booth.py
+++ Practice/class/booth.py
@@ -17,11 +17,11 @@ class AtmMachine:
         if user_input == "1":
             self.create_pin()
         elif user_input == "2":
-            pass
+            self.change_pin()
         elif user_input == "3":
-            pass
+            self.check_balance()
         elif user_input == "4":
-            pass
+            self.balance_withdraw()
         else:
             exit()
     
@@ -35,9 +35,41 @@ class AtmMachine:
             self.balance = user_balance
             print("Balance Depostite successfully!")
         else:
-            print("PIN will be at least 4 character. Please try again!")
+            print("PIN will be at least 4 character. Please try again!")        
+        self.menu()
 
-        
+    def change_pin(self):
+        old_pin = input("Enter your old PIN : ")
+        if old_pin == self.pin:
+            new_pin = input("Enter your new PIN : ")
+            if len(new_pin) >= 4:
+                self.pin = new_pin
+                print("PIN created successfully!")
+            else:
+                print("PIN will be at least 4 character. Please try again!")
+        else:
+            print("Your PIN is incorrect, please try again!")
+        self.menu()
+
+    def check_balance(self):
+        user_pin = input("Enter your PIN : ")
+        if user_pin == self.pin:
+            print(f"Your Balance is BDT-{self.balance}")
+        else:
+            print("Your PIN is incorrect, Please try again!")
+        self.menu()
+
+    def balance_withdraw(self):
+        user_pin = input("Enter your PIN : ")
+        if user_pin == self.pin:
+            amount = float(input("Enter your amount to withdraw : "))
+            if amount <= self.balance:
+                self.balance -= amount
+                print(f"You have withdrawn BDT-{amount}. Your new balance is BDT-{self.balance}")
+            else:
+                print("Insufficient balance!")
+        else:
+            print("Your PIN is incorrect, Please try again!")
         self.menu()
 
 atm = AtmMachine()
