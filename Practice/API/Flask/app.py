2de19b814cb73dbc93d3aed98a3998694975d8f7
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-01-02 17:03:28 +0600

    main file

--- Practice/API/Flask/app.py
+++ Practice/API/Flask/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__) #flask server initialization
 def home():
     return "Hello flask"
 
+@app.route("/welcome")
+def welcome():
+    return "welcome to the Flask Application"
+
+@app.route("/success/<int:score>")
+def success(score):
+    return f"The person is passed and the score is {score}"
+
+@app.route("/fail/<int:score>")
+def fail(score):
+    return f"The person is failed and the score is {score}"
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
