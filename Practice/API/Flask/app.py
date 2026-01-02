7a06796a27dfc4b69b3870f0e2ff73bf1f676900
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-01-02 17:17:50 +0600

    updated

--- Practice/API/Flask/app.py
+++ Practice/API/Flask/app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) #flask server initialization
 
 @app.route("/")
-def home():
-    return "Hello flask"
+def index():
+    return render_template("index.html")
 
 @app.route("/welcome")
 def welcome():
     return "welcome to the Flask Application"
+
+
 
 @app.route("/success/<int:score>")
 def success(score):
