eb8d3e02e79a29923e6f5989ba5923fd8b752927
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-01-04 21:28:51 +0600

    main file

--- Practice/API/FastAPI/main.py
+++ Practice/API/FastAPI/main.py
@@ -12,11 +12,37 @@ class Tea(BaseModel):
 
 teas : List[Tea] = []
 
+
 @app.get("/")
 def read_root():
     return {"message" : "welcome to the Tea House"}
 
-@app.get("/teas")
+
+@app.post("/addteas")
+def add_tea(tea : Tea):
+    teas.append(tea)
+    return tea
+
+
+@app.put("/updatetea/{tea_id}")
+def update_tea(tea_id : int, updated_tea : Tea):
+    for index, tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index] = updated_tea
+            return updated_tea
+    return {"error": "Tea not found"}
+
+
+@app.delete("/deletetea/{tea_id}")
+def delete_tea(tea_id : int):
+    for index, tea in enumerate(teas):
+        if tea_id == tea_id:
+            deleted_tea = teas.pop(index)
+            return deleted_tea
+    return {"error" : "Tea not found"}
+
+
+@app.get("/getteas")
 def get_teas():
     return teas
 
