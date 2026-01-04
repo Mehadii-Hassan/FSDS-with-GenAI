2fab61a4b35f60502b4a9c5d8e5f3c0802136bb8
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-01-04 20:33:22 +0600

    updated

--- Practice/API/FastAPI/main.py
+++ Practice/API/FastAPI/main.py
@@ -5,12 +5,21 @@ import uvicorn
 
 app = FastAPI() #server initilization
 
-teas = []
+class Tea(BaseModel):
+    id : int
+    name : str
+    origin : str 
+
+teas : List[Tea] = []
 
 @app.get("/")
 def read_root():
     return {"message" : "welcome to the Tea House"}
 
+@app.get("/teas")
+def get_teas():
+    return teas
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
