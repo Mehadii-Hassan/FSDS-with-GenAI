a13a70c45d49f839ea292473a254fc973a32f3e6
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-01-04 20:13:14 +0600

    updated

--- Practice/API/FastAPI/main.py
+++ Practice/API/FastAPI/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+import uvicorn
 
 app = FastAPI() #server initilization
 
+teas = []
+
 @app.get("/")
 def read_root():
-    return {"Hello" : "world"}
+    return {"message" : "welcome to the Tea House"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
