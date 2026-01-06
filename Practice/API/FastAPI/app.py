c3d913f2c31b3d4c2d23411c93f348d6de8dd513
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-01-06 23:11:49 +0600

    main file (ui)

--- Practice/API/FastAPI/app.py
+++ Practice/API/FastAPI/app.py
@@ -57,5 +57,5 @@ def get_teas():
     return teas
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
 
