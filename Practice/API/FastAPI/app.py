045ac821c158dd6885ce0e70059a1fec835ed254
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-01-06 23:05:25 +0600

    ui

--- Practice/API/FastAPI/app.py
+++ Practice/API/FastAPI/app.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 import uvicorn
+import os
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI() #server initilization
+
+
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+if os.path.exists(static_dir):
+    app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
 
 class Tea(BaseModel):
     id : int
